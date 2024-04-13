37f5892e4b5ef3ab086357d7e723636cb8130292
Prem Kathayat <132916774+premkathayatpk@users.noreply.github.com>
2024-04-13 14:22:19 +0000

    ad h

---
+++ product.php
@@ -0,0 +1,23 @@
+<?php
+require ('header.php');
+include '../config.php';
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes" />
+    <meta charset="UTF-8" />
+    <!-- The above 3 meta tags must come first in the head -->
+
+    <title>Save product details</title>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" type="text/javascript"></script>
+    <link rel="stylesheet" type="text/css" href="product.css">
+       
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+</head>
